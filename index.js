cc9d101b3abb380144d551f3a78e3d7ecab1c056
Andrei Dzugan <mr.dzugan@gmail.com>
2020-07-15 23:19:44 +0300

    Refactor code

--- index.js
+++ index.js
@@ -1,55 +1,61 @@
 'use strict';
 
 class Stack {
-    constructor(maxSize = 1000) {
-        this._size = 0;
-        this._maxSize = maxSize;
-    }
+	constructor(maxSize = 1000) {
+		this._size = 0;
+		this._maxSize = maxSize;
+	}
 
-    get size() {
-        return this._size;
-    }
+	get size() {
+		return this._size;
+	}
 
-    push(v) {
-        this[this._size++] = v;
-        if (this.size > this._maxSize) {
-            throw new RangeError('Stack overflow');
-        }
-        return this._size;
-    }
+	push(v) {
+		this[this._size++] = v;
+		if (this.size > this._maxSize) {
+			throw new RangeError('Stack overflow');
+		}
+		return this._size;
+	}
 
-    pop() {
-        if (!this._size) {
-            return;
-        }
-        const value = this[--this._size]
-        delete this[this._size];
-        return value;
-    }
+	pop() {
+		if (!this._size) {
+			return;
+		}
+		const value = this[--this._size]
+		delete this[this._size];
+		return value;
+	}
 
-    pick() {
-        return this[this._size - 1];
-    }
+	pick() {
+		return this[this._size - 1];
+	}
 }
 
 let stack = new Stack();
 
-const str = '()(()';
-
-function Check(str) {
-    for (const symb of str) {
-        if (symb === ')' && stack.size === 0) {
-            return false;
-        }
-        if (symb === '(') {
-            stack.push(symb);
-        } else if (symb === ')') {
-            if (stack.pick() === '(') {
-                stack.pop();
-            }
-        }
-    }
-    return stack.size === 0;
+/**
+ *
+ * @param str - any string with brackets
+ * @returns {boolean} is the brackets make up the correct sequence
+ * @constructor - ? // Артур, где тут конструктор? :D (автоматом документирует)
+ */
+function isCorrectBracketSequence(str) {
+	for (const elem of str) {
+		if (elem === ')' && stack.size === 0) {
+			return false;
+		}
+		if (elem === '(') {
+			stack.push(elem);
+		} else if (elem === ')') {
+			if (stack.pick() === '(') {
+				stack.pop();
+			}
+		}
+	}
+	return stack.size === 0;
 }
 
-console.log(Check(str));
+const str = '((((((()()()))()';
+
+console.log(isCorrectBracketSequence(str));
